07c0fc22176cf1d6ff0ad83d94a05c747ce0db68
Andrii Rybak <boghtml@gmail.com>
2025-02-06 09:50:45 +0200

    serializer update

--- apps/bookings/serializers.py
+++ apps/bookings/serializers.py
@@ -3,12 +3,15 @@ from rest_framework import serializers
 from .models import Booking
 
 class BookingSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Booking
         fields = [
             'id', 'restaurant_table', 'user', 'status',
             'booking_time', 'created_at', 'updated_at'
         ]
-        read_only_fields = ['id', 'created_at', 'updated_at']
-
-# Add comment on develop branch
+        read_only_fields = [
+            'id', 'user', 'status',
+            'created_at', 'updated_at'
+        ]
